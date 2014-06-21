ca2130bb71cff204ebd9422a8eb7ea9259e550ba
jbt <jeanabptisteth@free.fr>
2014-06-21 19:20:46 +0200

    le projet

---
+++ PhoneSendEmail8.1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneSendEmail8._1.Resources;
+
+namespace PhoneSendEmail8._1
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ PhoneSendEmail8.1/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneSendEmail8._1.Resources;
+using Microsoft.Phone.Tasks;
+
+namespace PhoneSendEmail8._1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        /// <summary>
+        /// Lors du clique sur le bouton rechercher un contacte
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            EmailAddressChooserTask emailSearch = new EmailAddressChooserTask();
+            emailSearch.Completed += emailSearch_Completed;
+            emailSearch.Show();
+        }
+
+        /// <summary>
+        /// Lors de la fin de l'événement de la recherche d'un contact
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void emailSearch_Completed(object sender, EmailResult e)
+        {
+            //teste une erreur
+            if (e.Error != null)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Error.Message);
+            }
+            else
+            {
+                //récupération du contact
+                var contactEmail = e.Email;
+                var contactName = e.DisplayName;
+                this.txtEmail.Text = contactEmail;
+                this.txtName.Text = contactName;
+            }
+        }
+
+        /// <summary>
+        /// Envoi un mail a la personne 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendMail_Click(object sender, RoutedEventArgs e)
+        {
+            EmailComposeTask emailCompose = new EmailComposeTask();
+            emailCompose.Body = "Mon corps de mail a envoyé.";
+            emailCompose.To = this.txtEmail.Text;//récupération de l'adresse via le champ TextBlock
+            emailCompose.Subject = "Email de test";
+            emailCompose.Show();
+        }
+    }
+}
